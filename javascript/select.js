4d6e591a7f414b14bddce37f9a26e041ab10fb99
Eric <huny3410@naver.com>
2023-08-31 16:10:43 +0900

    design: 사용자 plan 입력창과 로딩바 구현

---
+++ javascript/select.js
@@ -0,0 +1,31 @@
+const dateStartSelectBox = () => {
+    const dateArray = []
+
+    for (let i=6; i<12; i++) {
+        dateArray.push(`오전 ${i}시`)
+    }
+    dateArray.push('오후 12시');
+    for(let i=1; i<12; i++) {
+        dateArray.push(`오후 ${i}시`)
+    }
+
+    const selectBoxDateStart = document.querySelector('#form-select-date-start');
+
+    const selectBoxDateEnd = document.querySelector('#form-select-date-end');
+    
+    dateArray.forEach((item) => {
+        const optionsStart = document.createElement('option')
+        optionsStart.innerText = item
+        optionsStart.value = item
+
+        const optionsEnd = document.createElement('option')
+        optionsEnd.innerText = item
+        optionsEnd.value = item
+        
+        selectBoxDateStart.appendChild(optionsStart)
+        selectBoxDateEnd.appendChild(optionsEnd)
+    });
+    
+}
+
+dateStartSelectBox();
