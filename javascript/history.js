81bf132ee37532f3140795e5fdb9a864807d2ac8
Eric <huny3410@naver.com>
2023-09-03 22:52:28 +0900

    feat: 히스토리 삭제기능 구현

--- javascript/history.js
+++ javascript/history.js
@@ -7,34 +7,55 @@ const getHistory = () => {
         const json = localStorage.getItem(item)
         const value = JSON.parse(json)
         const historyItem = document.createElement('div')
-        historyItem.innerText = value["date"]
-        historyItem.className = "card"
+        historyItem.innerText = value["date"] + " - " + value["place"]
+        historyItem.className = "card history-title"
         historyContainer.appendChild(historyItem)
 
         historyItem.addEventListener('click', (e) => {
             $('#history').fadeOut(500,() => {
                 const $question = document.querySelector('.history-detail-question')
-
                 const $answer = document.querySelector('.history-detail-answer')
 
                 $question.innerText = value['question']
-
                 $answer.innerText = value['answer']
 
+                $('#historyKey').val(item)
                 $('#history-detail').fadeIn(500)
             })
-
         })
+
+
     })    
 }
 
-const closeBtnTapped= () => {
+const refreshHistory = () => {
+    document.querySelectorAll('.history-title').forEach(item => item.remove())
+    getHistory()
+}
+
+const closeBtnTapped = () => {
     $('#close-btn').click(() => {
         $('#history-detail').fadeOut(500,() => {
             $('#history').fadeIn(500)
+            $('#historyKey').val('')
         })
+    })
+}
+
+const deleteBtnTapped = () => {
+    $('#delete-btn').click(() => {
+        const result = confirm('정말 삭제하시겠어요?')
+        const historyKey = $('#historyKey').val()
+        if(result) {
+            localStorage.removeItem(historyKey)
+            refreshHistory()
+            $('#close-btn').trigger('click')
+        } else {
+            return
+        }
     })
 }
 
 getHistory();
 closeBtnTapped()
+deleteBtnTapped()
